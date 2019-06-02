c9da99dc961b30c2e27b883de4934da3f3bd6430
Miha Sustersic <miha.sustersic.work@gmail.com>
2019-06-02 17:34:00 +0200

    Moved publish date, line breaks to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 
+import metadata from "./data/metadata.json";
+
 import PersonalInfo from "./components/PersonalInfo";
+import Education from "./components/Education";
+
+import { StyledHr } from "./styledComponents/common";
 
 const StyledContainer = styled.div`
 	max-width: 1024px;
 	margin: 0 auto;
+	padding: 1rem;
 	@media screen and (min-width: 768px) {
 		padding: 1rem 53px;
 	}
 `;
 
+const DateParagraph = styled.p`
+	font-family: "Open Sans", sans-serif;
+	font-size: 1rem;
+	font-weight: 500;
+	margin: 0;
+	margin-bottom: 0.5rem;
+	text-align: right;
+	padding-right: 1rem;
+	padding-top: 1rem;
+`;
+
 const App: React.FC = () => {
 	return (
 		<StyledContainer>
+			<DateParagraph>{metadata.publishDate}</DateParagraph>
+			<StyledHr />
 			<PersonalInfo />
+			<StyledHr />
+			<Education />
 		</StyledContainer>
 	);
 };
--- src/components/PersonalInfo/index.tsx
+++ src/components/PersonalInfo/index.tsx
@@ -9,7 +9,6 @@ import mailIcon from "./mailIcon.svg";
 
 // data
 import personalInfo from "../../data/personalInfo.json";
-import metadata from "../../data/metadata.json";
 
 // styled components
 import {
@@ -23,22 +22,10 @@ const HeaderContainer = styled.div`
 	align-items: center;
 	justify-content: flex-start;
 	flex-wrap: wrap;
-	padding: 1rem;
 `;
 
 const PersonalInfoContainer = styled.div`
 	margin-left: 1rem;
-`;
-
-const DateParagraph = styled.p`
-	font-family: "Open Sans", sans-serif;
-	font-size: 1rem;
-	font-weight: 500;
-	margin: 0;
-	margin-bottom: 0.5rem;
-	text-align: right;
-	padding-right: 1rem;
-	padding-top: 1rem;
 `;
 
 const StyledIcon = styled.img`
@@ -53,29 +40,24 @@ const PotraitImage = styled.img`
 
 const PersonalInfo: React.FC = () => {
 	return (
-		<React.Fragment>
-			<DateParagraph>{metadata.publishDate}</DateParagraph>
-			<StyledHr />
-			<HeaderContainer>
-				<PotraitImage src={headshot} alt="portrait" />
-				<PersonalInfoContainer>
-					<StyledHeading>{personalInfo.name}</StyledHeading>
-					<StyledParagraph>
-						<StyledIcon src={cakeIcon} alt="birthday-cake" />
-						{`: ${personalInfo.birthDate}`}
-					</StyledParagraph>
-					<StyledParagraph>
-						<StyledIcon src={homeIcon} alt="birthday-cake" />
-						{`: ${personalInfo.address}`}
-					</StyledParagraph>
-					<StyledParagraph>
-						<StyledIcon src={mailIcon} alt="birthday-cake" />
-						{`: ${personalInfo.email}`}
-					</StyledParagraph>
-				</PersonalInfoContainer>
-			</HeaderContainer>
-			<StyledHr />
-		</React.Fragment>
+		<HeaderContainer>
+			<PotraitImage src={headshot} alt="portrait" />
+			<PersonalInfoContainer>
+				<StyledHeading>{personalInfo.name}</StyledHeading>
+				<StyledParagraph>
+					<StyledIcon src={cakeIcon} alt="birthday-cake" />
+					{`: ${personalInfo.birthDate}`}
+				</StyledParagraph>
+				<StyledParagraph>
+					<StyledIcon src={homeIcon} alt="birthday-cake" />
+					{`: ${personalInfo.address}`}
+				</StyledParagraph>
+				<StyledParagraph>
+					<StyledIcon src={mailIcon} alt="birthday-cake" />
+					{`: ${personalInfo.email}`}
+				</StyledParagraph>
+			</PersonalInfoContainer>
+		</HeaderContainer>
 	);
 };
 
