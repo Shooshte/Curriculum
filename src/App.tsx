32bc84df720844e7e35a314153f45001aa02e10b
Miha Sustersic <miha.sustersic.work@gmail.com>
2019-06-23 20:02:26 +0200

    Put skills in front of work experience.

--- src/App.tsx
+++ src/App.tsx
@@ -11,40 +11,40 @@ import Skills from "./components/Skills";
 import { StyledHr } from "./styledComponents/common";
 
 const StyledContainer = styled.div`
-	max-width: 1024px;
-	margin: 0 auto;
-	padding: 1rem;
-	@media screen and (min-width: 768px) {
-		padding: 1rem 53px;
-	}
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 1rem;
+  @media screen and (min-width: 768px) {
+    padding: 1rem 53px;
+  }
 `;
 
 const DateParagraph = styled.p`
-	font-family: "Open Sans", sans-serif;
-	font-size: 1rem;
-	font-weight: 500;
-	margin: 0;
-	margin-bottom: 0.5rem;
-	text-align: right;
-	padding-right: 1rem;
-	padding-top: 1rem;
+  font-family: "Open Sans", sans-serif;
+  font-size: 1rem;
+  font-weight: 500;
+  margin: 0;
+  margin-bottom: 0.5rem;
+  text-align: right;
+  padding-right: 1rem;
+  padding-top: 1rem;
 `;
 
 const App: React.FC = () => {
-	return (
-		<StyledContainer>
-			<DateParagraph>{metadata.publishDate}</DateParagraph>
-			<StyledHr />
-			<PersonalInfo />
-			<StyledHr />
-			<Education />
-			<StyledHr />
-			<WorkExperience />
-			<StyledHr />
-			<Skills />
-			<StyledHr />
-		</StyledContainer>
-	);
+  return (
+    <StyledContainer>
+      <DateParagraph>{metadata.publishDate}</DateParagraph>
+      <StyledHr />
+      <PersonalInfo />
+      <StyledHr />
+      <Education />
+      <StyledHr />
+      <Skills />
+      <StyledHr />
+      <WorkExperience />
+      <StyledHr />
+    </StyledContainer>
+  );
 };
 
 export default App;
