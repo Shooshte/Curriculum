73ca7d4e3c61fbd3b6e966023aad96471f9c95eb
Miha Sustersic <miha.sustersic.work@gmail.com>
2019-06-23 19:58:21 +0200

    Updates

---
+++ precache-manifest.cfc467413ce0ba849fb14798cede66fd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "53eb0e60bcac83e4323e",
+    "url": "/curriculum/static/css/main.544f570f.chunk.css"
+  },
+  {
+    "revision": "53eb0e60bcac83e4323e",
+    "url": "/curriculum/static/js/main.cbe1fc34.chunk.js"
+  },
+  {
+    "revision": "acad9920e4b64e8f50f7",
+    "url": "/curriculum/static/js/runtime~main.90aab327.js"
+  },
+  {
+    "revision": "1d2c92120afac95aec84",
+    "url": "/curriculum/static/js/2.b17902c2.chunk.js"
+  },
+  {
+    "revision": "19151d585f6806824189b5a23eea8cce",
+    "url": "/curriculum/static/media/Cogs_icon_blue.19151d58.svg"
+  },
+  {
+    "revision": "9b9f80fb887ab0a629e59ec096bbeba6",
+    "url": "/curriculum/static/media/Cogs_icon_gray.9b9f80fb.svg"
+  },
+  {
+    "revision": "b855797128c5f01acc429cf03e407444",
+    "url": "/curriculum/static/media/headshot.b8557971.png"
+  },
+  {
+    "revision": "1265b2cf14902ded74a1d79386056a8d",
+    "url": "/curriculum/static/media/cakeIcon.1265b2cf.svg"
+  },
+  {
+    "revision": "2a28fb229a125b450a5a26b31d05abdc",
+    "url": "/curriculum/static/media/homeIcon.2a28fb22.svg"
+  },
+  {
+    "revision": "4fe6cbb9e722623eba3af9f90eaed8f6",
+    "url": "/curriculum/static/media/mailIcon.4fe6cbb9.svg"
+  },
+  {
+    "revision": "c0c1fabbba174c27e6b1eeb7c908ab92",
+    "url": "/curriculum/index.html"
+  }
+];
